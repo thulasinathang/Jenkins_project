02b8162d1c78c9e405770003e1bae81846d7c244
thulasinathang <thulasinathan007@gmail.com>
2023-04-15 16:11:40 -0400

    Auto web hook

--- Testing.java
+++ Testing.java
@@ -2,7 +2,7 @@ public class Testing {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Testing  Jenking job2  manually");
+		System.out.println("Testing  Jenking job3 auto webhooks");
 
 	}
 
