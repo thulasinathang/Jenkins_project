a800ec63b4ecb27fcce4c14ab121c5b1812e610b
thulasinathang <thulasinathan007@gmail.com>
2023-04-15 16:18:40 -0400

    new webhooks url updated

--- Testing.java
+++ Testing.java
@@ -2,7 +2,7 @@ public class Testing {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Testing  Jenking job3 auto webhooks");
+		System.out.println("Testing  Jenking job3 auto using webhooks");
 
 	}
 
